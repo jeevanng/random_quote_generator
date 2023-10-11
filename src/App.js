33cf26ee474473df5df83258316d736a83f23738
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-10-12 10:16:15 +1100

    Random quote generator component button added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import RandomQuoteGenerator from './components/RandomQuoteGenerator';
 
 function App() {
+
   return (
     <div className="App">
       <h1>Random Quote Generator</h1>
+      <RandomQuoteGenerator />
     </div>
   );
 }
---
+++ src/components/RandomQuoteGenerator.jsx
@@ -0,0 +1,10 @@
+
+
+export default function RandomQuoteGenerator(){
+
+    return(
+        <div>
+            <button>Click Me</button>
+        </div>
+    )
+}
