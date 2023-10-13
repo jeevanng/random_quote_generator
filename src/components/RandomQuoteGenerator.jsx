9b5918e8e7758741706423f3110c88ba642ca066
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-10-13 12:33:54 +1100

    Added comments to pages

--- src/components/RandomQuoteGenerator.jsx
+++ src/components/RandomQuoteGenerator.jsx
@@ -7,9 +7,13 @@ import LoadingImage from "./LoadingImage";
 export default function RandomQuoteGenerator(){
 
     // Loading state
+    // Start with a default value of true
+    // That is, loading is occurring
     const [loading, setLoading] = useState(true);
 
     // Error state
+    // Default state of null
+    // No errors initially
     const [error, setError] = useState(null);
 
     // Search results
@@ -17,8 +21,10 @@ export default function RandomQuoteGenerator(){
     const [author, setAuthor] = useState([]);
 
     // Api URL
+    // Destructure api variable from the context. Accesses {api} value from ApiContext 
     const {api} = useContext(ApiContext);
 
+    // Run once, immediately after the component is rendered
     useEffect(() => {
         console.log("Making a fetch request")
         getRandomQuote();
@@ -26,17 +32,27 @@ export default function RandomQuoteGenerator(){
     // eslint-disable-next-line    
     }, []);
 
+    // Async function outside useEffect for easier debugging
     async function getRandomQuote(){
         try{
+            // set loading state to be true before making fetch request
+            // Indicate that component is in loading state
             setLoading(true);
+            // make network request to the api 
+            // wait for response
             let response = await fetch(api);
+            // read response data as json, wait for data to be parsed
             let responseData = await response.json();
+            // set 'quote' and 'author' state variables with data fetched from the api
             setQuote(responseData[0].content);
             setAuthor(responseData[0].author);
 
+            // Set a timeout of 1 second (minimum) to change the 'loading' state variable to false
+            // Brief loading delay to allow user to see loading animation
             setTimeout(() =>{
                 setLoading(false);
             }, 1000);
+            // Catch any errors, set 'error' state to the error object
         } catch(error){
             console.error(error);
             setError(error);
@@ -44,12 +60,20 @@ export default function RandomQuoteGenerator(){
     }
 
     return (
+        // Container is bootstrap UI for styling
         <Container className="quote-container">
+            {/* Conditional Rendering */}
+            {/* If there is an error, display an error message. OTHERWISE display main content */}
             {error ? (
                 <div>
                     <p>Error: {error.message}</p>
                 </div>
             ) : (
+                // This will display if there is no error
+                // Another conditional rendering
+                // Reads loading state
+                // If true, render the 'LoadingImage' component
+                // If false, load the main content
                 loading ? (<LoadingImage />) : (
                 <figure className="text-center">
                     <blockquote className="blockquote">
--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,12 +1,16 @@
 import { createContext, useState } from "react";
 
+
 export const ApiContext = createContext(null)
 
 export default function ApiProvider({children}){
     
+    // Start with default URL
     const [apiUrl, setApiUrl] = useState("https://api.quotable.io/quotes/random");
 
     return(
+        // Provides the context values to it's descendants/children. Sets value prop to an object with two properties
+        // Components nested within this context can access and modify the Api URL as needed. 
         <ApiContext.Provider value={
             {
                 api: apiUrl, 
